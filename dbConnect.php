a08424e58532a011625204b70871a479f692a5b4
imran <30371031+imranlatur24@users.noreply.github.com>
2017-09-20 22:38:43 +0530

    Add files via upload

---
+++ dbConnect.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','root');
+ define('PASS','imran786');
+ define('DB','simpli');
+ 
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
